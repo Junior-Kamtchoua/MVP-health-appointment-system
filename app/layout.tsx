c41e0663581829a5ba56ccdb73a6e71dbab567da
Junior-Kamtchoua <kamtchouajunior@gmail.com>
2026-01-20 04:05:47 +0100

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,10 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "HealthTrack",
   description: "Healthcare appointment booking system",
+  icons: {
+    icon: "/favicon.png",
+    apple: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
